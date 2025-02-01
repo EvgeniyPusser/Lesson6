14b2c5cfaab38969d2572180ed9ecb911a7ff4a7
Evgeniy <evgeniipusser88@gmail.com>
2025-02-01 23:17:02 +0200

    in old maner with Time delay

--- src/script.js
+++ src/script.js
@@ -1,46 +1,3 @@
-// const detailedImages = document.querySelector(".dImage");
-
-// const detailedTitle = document.querySelector(".detailed-title");
-
-// const boxesElements = document.querySelectorAll(".gallery--item");
-
-
-// // function setDetails(boxElement) {
-// //   detailedImages.src = boxElement.querySelector(".gallery--item_image").src;
-// //   // detailedTitle.innerHTML = boxElement.getAttribute(".data-detailed-text");
-// //   console.log("Data text:", boxElement.getAttribute(".data-detailed-text"));
-// //   console.log("Clicked element:", boxElement);
-// // console.log("Text attribute:", boxElement.getAttribute("data-detailed-text"));
-// //   detailedTitle.innerHTML = boxElement.getAttribute(".data-detailed-text");
-// // }
-
-// function setDetails(boxElement) {
-//   detailedImages.src = boxElement.querySelector(".gallery--item_image").src;
-
-//   // Find the element that has the data attribute
-//   const detailsElement = boxElement.querySelector(".details");
-
-//   if (detailsElement) {
-//     detailedTitle.innerHTML = detailsElement.getAttribute("data-detailed-text");
-//   } else {
-//     console.error("No element with 'data-detailed-text' found inside", boxElement);
-//   }
-
-//    gsap.fromTo(detailedImages, { opacity: 0 }, { opacity: 1, duration: 0.5 });
-//     gsap.fromTo(detailedTitle, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 });
-// }
-
-
-
-
-
-// for (let i = 0; i < boxesElements.length; i++) {
-//   boxesElements[i].addEventListener("click", function () {
-//     setDetails(boxesElements[i]);
-//   });
-
-// }
-
 const detailedImages = document.querySelector(".dImage");
 const detailedTitle = document.querySelector(".detailed-title");
 const boxesElements = document.querySelectorAll(".gallery--item");
@@ -51,27 +8,28 @@ function setDetails(boxElement) {
 
   if (imageElement) {
     detailedImages.src = imageElement.src;
-  
-    detailedImages.classList.remove("animation-image"); // Remove first
-    void detailedImages.offsetWidth; // Trigger reflow
-    detailedImages.classList.add("animation-image");
+    detailedImages.classList.remove("animation-image");
+
+    setTimeout(function () {
+      detailedImages.classList.add("animation-image"); 
+    }, 10); 
   }
 
   if (titleElement) {
     detailedTitle.innerHTML = titleElement.dataset.detailedText;
-  
-    detailedTitle.classList.remove("animation-text"); // Remove first
-    void detailedTitle.offsetWidth; // Trigger reflow
-    detailedTitle.classList.add("animation-text");
   }
+
+  detailedTitle.classList.remove("animation-text");
+
+  setTimeout(function () {
+    detailedTitle.classList.add("animation-text");
+  }, 10);
 }
-  
 
 
-
-boxesElements.forEach((box) => {
-  box.addEventListener("click", function () {
-    setDetails(box);
-   
+for (let i = 0; i < boxesElements.length; i++) {
+  boxesElements[i].addEventListener("click", function () {
+    setDetails(boxesElements[i]);
   });
-});
+}
+
